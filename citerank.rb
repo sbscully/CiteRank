1d6887bd1b1014a6e99a189eed0a4d35e00f2403
Samuel Scully <Sam@samuel-scullys-computer.local>
2010-12-05 16:44:35 +0800

    OOified

--- citerank.rb
+++ citerank.rb
@@ -1,127 +1,173 @@
 require 'bigdecimal'
 
-def cite_forward(file)
-  forw = {}
-  links = cite_array(file)
-  links.each do |link|
-    from = link[0] ; to = link[1]
-    forw[from] ? forw[from] << to : forw[from] = [to]
+class Citations
+  def initialize(file)
+    @links = generate_links_array(file)
   end
-  remove_dangling_links(forw, links)
-end
-
-def back_links(file)
-  back = {}
-  cite_array(file).each do |link|
-    from = link[0] ; to = link[1]
-    back[to] ? back[to] << from : back[to] = [from]
-  end
-  back
-end
-
-def remove_dangling_links(hash, links)
-  forw = {}
-  links.each do |link|
-    from = link[0] ; to = link[1]
-    if hash.include? to
+  
+  def forward
+    forw = {}
+    @links.each do |link|
+      from = link[0] ; to = link[1]
       forw[from] ? forw[from] << to : forw[from] = [to]
     end
+    remove_dangling_links(forw)
   end
-  forw
+
+  def backward
+    back = {}
+    @links.each do |link|
+      from = link[0] ; to = link[1]
+      back[to] ? back[to] << from : back[to] = [from]
+    end
+    back
+  end
+  
+  private
+
+  def remove_dangling_links(hash)
+    forw = {}
+    @links.each do |link|
+      from = link[0] ; to = link[1]
+      if hash.include? to
+        forw[from] ? forw[from] << to : forw[from] = [to]
+      end
+    end
+    forw
+  end
+
+  def generate_links_array(file)
+    cite_array = []
+    File.open(file) do |citation_data|
+      citation_data.each do |line|
+        from, to = line.split(' ')
+        cite_array << [from.to_i, to.to_i]  
+      end
+    end
+    cite_array
+  end
 end
 
-def cite_array(file)
-  cite_array = []
-  File.open(file) do |citation_data|
-    citation_data.each do |line|
-      from, to = line.split(' ')
-      cite_array << [from.to_i, to.to_i]  
+class TransferMatrix 
+  def initialize(citations_hash={})
+    @matrix = Hash.new(0)
+    if citations_hash
+      citations_hash.each { |paper,citations| add_paper([paper, citations]) }
     end
   end
-  cite_array
-end
-
-def rank_init(file)
-  rank = {}
-  citations = cite_forward(file)
-  citations.each_key { |k| rank[k] = BigDecimal((1/citations.length).to_s) }
-  rank
-end
-
-def transfer_matrix(file)
-  forward = cite_forward(file)
-  columns = {}
-  rows = {}
   
-  forward.each do |i, f|
-    columns[i] = 1.0 / forward[i].count
+  def [](row, column)
+    @matrix[[row, column]]
   end
   
-  forward.each do |k, v|
-    v.each { |paper| rows[paper] ? rows[paper] << [k,columns[k]] : rows[paper] = [ [k,columns[k]] ] }
+  def add_paper(array)
+    paper = array[0] ; citations = array[1]
+    citations.each { |citation| @matrix[[paper, citation]] = 1.0 / citations.count }
   end
- 
-  rows
-end
+  
+  alias << add_paper
 
-def sparse_vector_times(value, r)
-  value.reduce(0) { |tot, v| tot += (r[v[0]] || 0)*v[1] }
-end
-
-def sparse_matrix_vector_times(m, r)
-  new_r = {}
-  m.each do |key, value|
-    new_r[key] = sparse_vector_times(value , r)
+  def multiply_rank_vector(rank_vector)
+    output_vector = Hash.new(0)
+    @matrix.each do |i, v|
+      output_vector[i[1]]  += (rank_vector[i[0]]) * v
+    end
+    RankVector.new(:values => output_vector)
   end
-  new_r
 end
 
-def rank_pages(file, diff, d)
-  w = transfer_matrix(file)
-  rank = rank_init(file)
-  
-  nodes = rank.count
-  
-  e = 10000
-  while e.abs > diff
-    new_rank = {}
-    sparse_matrix_vector_times(w, rank).each { |k,v| new_rank[k] = d*v + (1-d) / nodes }
-    e = (rank.values.reduce(:+) - new_rank.values.reduce(:+))
-    rank = new_rank
+class RankVector
+  def initialize(options={})
+    @vector = options[:values] || Hash.new(0)
+    default_values(options[:default_values]) if options[:default_values]
   end
-  rank
-end
-
-def top_ten
-  rank = rank_pages("hep-th-citations", 1e-12, 0.5)
-  values = rank.values.sort
-  top10 = values.reverse[0..10]
   
-  citations = back_links("hep-th-citations") 
-  
-  top10.each do |r|
-    paper = rank.select { |k,v| v == r }[0][0]
-    puts "\n #{paper.to_s} | #{normalise(r, values)*10} | #{citations[paper].count}"
-  end
-  nil
-end
-
-def normalise(value, set)
-  max = Math.log(set.max)
-  min = Math.log(set.min)
-  BigDecimal( ((Math.log(value) - min) / (max - min)).to_s ).round(5).to_f 
-end
-
-def date_hash(file)
-  hash = {}
-  File.open(file) do |citation_data|
-    citation_data.each do |line|
-      paper, date = line.split(' ')
-      hash[paper.to_i] = date
+  def default_values(citations_hash)
+    citations_hash.each_key do |k| 
+      @vector[k] = BigDecimal('1')/citations_hash.length
     end
   end
-  hash
+  
+  def sum
+    @vector.values.reduce(:+)
+  end
+  
+  def sort
+    sorted = []
+    @vector.each { |k,v| sorted << [v,k] }
+    sorted.sort.reverse.map { |a| a[0], a[1] = a[1], a[0] }
+  end
+  
+  def normalise
+    normalised = Hash.new(0)
+    max, min = Math.log(@vector.values.max), Math.log(@vector.values.min)
+    @vector.each { |k,v| normalised[k] = ( Math.log(v) - min ) / ( max - min ) }
+    RankVector.new(:values => normalised)
+  end
+  
+  def add_paper(paper)
+    @vector[paper] ||= BigDecimal('1')/@vector.length
+  end
+  
+  alias << add_paper
+  
+  def [](index)
+    @vector[index]
+  end
+  
+  def []=(index, value)
+    @vector[index] = value
+  end
+  
+  def each(&block)
+    @vector.each(&block)
+  end
+  
+  def length
+    @vector.length
+  end
 end
+
+class CiteGraph
+  def initialize(file, options={})
+    @citations = Citations.new(file)
+    @d = options[:d] || 0.5
+    @accuracy = options[:accuracy] || 1e-10
+  end
+  
+  def rank
+    @rank ||= rank!
+  end
+  
+  def rank!
+    transfer_matrix = TransferMatrix.new(@citations.forward)
+    rank_vector = RankVector.new(:default_values => @citations.forward)
+    
+    e = 1
+    while e.abs > @accuracy
+      new_rank_vector = transfer_matrix.multiply_rank_vector(rank_vector)
+      new_rank_vector.each do |k,v| 
+        new_rank_vector[k] = @d*v + (1-@d) / rank_vector.length
+      end
+      e = (rank_vector.sum - new_rank_vector.sum)
+      rank_vector = new_rank_vector
+    end
+    @rank = rank_vector
+  end
+  
+  def top(number)
+    rank_vector = rank
+    top10 = rank_vector.normalise.sort[0..number]
+    cited_by = @citations.backward
+
+    top10.each do |r|
+      paper = r[0] ; rank = r[1]
+      puts "\n #{paper.to_s} | #{rank*10} | #{cited_by[paper].count} "
+    end
+    top10
+  end
+end
+
   
     
   
